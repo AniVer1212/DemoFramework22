152c9053ec6e526cc0d370482359a662e2ab80df
ANISHU <ANISHU@ANISHU>
2024-10-04 09:51:40 +0530

    committtt messageeeeee

---
+++ DemoGithub/src/test/java/leadTest/ToCreateOrganization.java
@@ -0,0 +1,8 @@
+package leadTest;
+
+public class ToCreateOrganization {
+
+	public static void main(String[] args) {
+		System.out.println("Organization created successfully");
+	}
+}
---
+++ DemoGithub/src/test/java/organizationTest/ToCreateLead.java
@@ -0,0 +1,8 @@
+package organizationTest;
+
+public class ToCreateLead {
+
+	public static void main(String[] args) {
+		System.out.println("Lead created successfully");
+	}
+}
